5ef266a21eabfabf0259522ec375d55f07ea1cf7
Collin2046 <keh229@pitt.edu>
2022-10-24 21:24:42 -0400

    Update hobby.jsx

--- src/pages/hobby.jsx
+++ src/pages/hobby.jsx
@@ -13,7 +13,7 @@ function Hobby() {
                
                
                <p>Dota 2 is a 2013 multiplayer online battle arena (MOBA) video game developed and published by Valve. The game is a sequel to Defense of the Ancients (DotA), which was a community-created mod for Blizzard Entertainment's Warcraft III: Reign of Chaos. Dota 2 is played in matches between two teams of five players, with each team occupying and defending their own separate base on the map. Each of the ten players independently controls a powerful character known as a "hero" that all have unique abilities and differing styles of play. During a match players collect experience points and items for their heroes to successfully defeat the opposing team's heroes in player versus player combat. A team wins by being the first to destroy the other team's "Ancient", a large structure located within their base.</p>
-               <h3>Let us  watch a promotion vedio and youd can dowload and try it <a href="https://www.dota2.com/home"><em className="here">here!</em></a></h3>
+               <h3>Let us  watch a promotion video and you can download and try it <a href="https://www.dota2.com/home"><em className="here">here!</em></a></h3>
                <div className="posVideo">
                <video width="750" height="500" controls >
       <source src="https://youtu.be/-cSFPIwMEq4" type="video/mp4"/>
